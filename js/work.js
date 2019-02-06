79fb31c80497c92c245cfd7e3a82417bb352b984
Phillip Patrick <phillip.n.patrick@gmail.com>
2019-02-06 11:19:01 -0700

    Work carousel fully functional

--- js/work.js
+++ js/work.js
@@ -4,31 +4,15 @@ var works = [
     pic: "./img/project-calculator.jpg",
     url: "https://frosty-banach-905bee.netlify.com/",
     description: "Calculator with a JavaScript backend that responds to the number pad.",
-    technology: ["bootstrap", "css3", "git", "github", "heroku", "html5", "javascript", "jquery"],
-    github: ""
+    technology: ["bootstrap", "css3", "heroku", "html5", "javascript", "jquery"],
+    github: "https://github.com/phillipnpatrick/calculator"
   },
   {
     title: "Home Knits",
     pic: "./img/home-knits-logo.jpg",
     url: "https://pnpcfproject.herokuapp.com/",
     description: "Ruby on Rails mock e-commerce site.",
-    technology: ["bootstrap", "css3", "git", "github", "heroku", "html5", "javascript", "jquery", "rails", "ruby"],
+    technology: ["bootstrap", "css3", "heroku", "html5", "javascript", "jquery", "rails", "ruby"],
     github: "https://github.com/phillipnpatrick/cf_project"
-  },
-  {
-    title: "Third Project",
-    pic: "./img/coming-soon.jpg",
-    url: "https://www.google.com",
-    description: "",
-    technology: [],
-    github: ""
-  },
-  {
-    title: "Fourth Project",
-    pic: "./img/coming-soon.jpg",
-    url: "https://www.google.com",
-    description: "",
-    technology: [],
-    github: ""
   }
 ];
