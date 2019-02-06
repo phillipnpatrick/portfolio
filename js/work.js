d0eceb3189defadde9a47ec0d6eabbf04d7d6059
Phillip Patrick <phillip.n.patrick@gmail.com>
2019-02-05 19:06:19 -0700

    Place work into carousel; add sidebar.

--- js/work.js
+++ js/work.js
@@ -2,21 +2,33 @@ var works = [
   {
     title: "Calculator",
     pic: "./img/project-calculator.jpg",
-    url: "https://frosty-banach-905bee.netlify.com/"
+    url: "https://frosty-banach-905bee.netlify.com/",
+    description: "Calculator with a JavaScript backend that responds to the number pad.",
+    technology: ["bootstrap", "css3", "git", "github", "heroku", "html5", "javascript", "jquery"],
+    github: ""
   },
   {
-    title: "Ruby on Rails Mock E-Commerce Site",
+    title: "Home Knits",
     pic: "./img/home-knits-logo.jpg",
-    url: "https://pnpcfproject.herokuapp.com/"
+    url: "https://pnpcfproject.herokuapp.com/",
+    description: "Ruby on Rails mock e-commerce site.",
+    technology: ["bootstrap", "css3", "git", "github", "heroku", "html5", "javascript", "jquery", "rails", "ruby"],
+    github: "https://github.com/phillipnpatrick/cf_project"
   },
   {
     title: "Third Project",
     pic: "./img/coming-soon.jpg",
-    url: "https://www.google.com"
+    url: "https://www.google.com",
+    description: "",
+    technology: [],
+    github: ""
   },
   {
     title: "Fourth Project",
     pic: "./img/coming-soon.jpg",
-    url: "https://www.google.com"
+    url: "https://www.google.com",
+    description: "",
+    technology: [],
+    github: ""
   }
 ];
